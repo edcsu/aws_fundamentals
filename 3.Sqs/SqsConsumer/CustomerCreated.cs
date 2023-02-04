18d7f6d8ebdf525f5824d64fb8e8872aa866e009
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-02-04 13:02:25 +0300

    feat: sqs consumer

---
+++ 3.Sqs/SqsConsumer/CustomerCreated.cs
@@ -0,0 +1,14 @@
+namespace SqsConsumer;
+
+public class CustomerCreated
+{
+    public  Guid Id { get; init; }
+    
+    public string FullName { get; init; } = default!;
+    
+    public  string Email { get; init; } = default!;
+    
+    public  string GithubUsername { get; init; } = default!;
+    
+    public  DateTime DateOfBirth { get; init; }
+}
