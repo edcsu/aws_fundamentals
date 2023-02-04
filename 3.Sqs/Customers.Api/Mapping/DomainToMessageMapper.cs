42908c9283573b0bc5a35ee40c5de1b49f0cdf5b
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-02-04 14:37:07 +0300

    feat: add publisher logic

---
+++ 3.Sqs/Customers.Api/Mapping/DomainToMessageMapper.cs
@@ -0,0 +1,33 @@
+using Customers.Api.Contracts.Data;
+using Customers.Api.Contracts.Messages;
+using Customers.Api.Domain;
+using Customers.Api.Messaging;
+
+namespace Customers.Api.Mapping;
+
+public static class DomainToMessageMapper
+{
+    public static CustomerCreated ToCustomerCreatedMessage(this Customer customer)
+    {
+        return new CustomerCreated
+        {
+            Id = customer.Id,
+            Email = customer.Email,
+            GitHubUsername = customer.GitHubUsername,
+            FullName = customer.FullName,
+            DateOfBirth = customer.DateOfBirth
+        };
+    }    
+    
+    public static CustomerUpdated ToCustomerUpdatedMessage(this Customer customer)
+    {
+        return new CustomerUpdated
+        {
+            Id = customer.Id,
+            Email = customer.Email,
+            GitHubUsername = customer.GitHubUsername,
+            FullName = customer.FullName,
+            DateOfBirth = customer.DateOfBirth
+        };
+    }
+}
