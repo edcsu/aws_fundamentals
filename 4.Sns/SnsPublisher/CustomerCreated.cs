243dd34fe581399deb08958860d355caac9c010c
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-02-05 12:06:19 +0300

    feat: implement sns publisher

---
+++ 4.Sns/SnsPublisher/CustomerCreated.cs
@@ -0,0 +1,10 @@
+namespace SnsPublisher;
+
+public class CustomerCreated
+{
+    public  Guid Id { get; init; }
+    public string FullName { get; init; } = default!;
+    public  string Email { get; init; } = default!;
+    public  string GithubUsername { get; init; } = default!;
+    public  DateTime DateOfBirth { get; init; }
+}
---
+++ 4.Sns/SnsPublisher/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.Json;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using SnsPublisher;
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "skeith@skesoft.co.ug",
+    FullName = "Ssewannonda Keith Edwin",
+    DateOfBirth = new DateTime(1991, 12, 1),
+    GithubUsername = "edcsu"
+};
+
+var snsClient = new AmazonSimpleNotificationServiceClient();
+
+var topicArnResponse = await snsClient.FindTopicAsync("customers");
+
+var publishRequest = new PublishRequest
+{
+    TopicArn = topicArnResponse.TopicArn,
+    Message = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await snsClient.PublishAsync(publishRequest);
+
+/*
+ // upto 10 batch entries
+ var publishBatchRequest = new PublishBatchRequest
+{
+    PublishBatchRequestEntries = new List<PublishBatchRequestEntry>
+    {
+        new PublishBatchRequestEntry
+        {
+            Id = Guid.NewGuid().ToString(),
+            Message = JsonSerializer.Serialize(customer),
+            MessageAttributes = new Dictionary<string, MessageAttributeValue>
+            {
+                {
+                    "MessageType", new MessageAttributeValue
+                    {
+                        DataType = "String",
+                        StringValue = nameof(CustomerCreated)
+                    }
+                }
+            }
+        }
+    },
+    TopicArn = topicArnResponse.TopicArn
+};
+
+var batchResponse = await snsClient.PublishBatchAsync(publishBatchRequest);*/
