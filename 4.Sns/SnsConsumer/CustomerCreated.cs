681cbbac3e65d0ca67ba8935c657aeb54afe9fdb
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-02-05 12:24:22 +0300

    feat: add sns consumer

---
+++ 4.Sns/SnsConsumer/CustomerCreated.cs
@@ -0,0 +1,14 @@
+namespace SnsConsumer;
+
+public class CustomerCreated
+{
+    public required Guid Id { get; init; }
+    
+    public required string FullName { get; init; }
+    
+    public  required string Email { get; init; }
+    
+    public  required string GithubUsername { get; init; }
+    
+    public  required DateTime DateOfBirth { get; init; }
+}
