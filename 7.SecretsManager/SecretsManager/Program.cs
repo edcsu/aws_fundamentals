f53831e4720ff341f5eb6470fe229ea0262aa80f
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-02-05 18:00:51 +0300

    feat: add get and describe secrets console app

---
+++ 7.SecretsManager/SecretsManager/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+var secretsManagerClient = new AmazonSecretsManagerClient();
+
+var request = new GetSecretValueRequest
+{
+    SecretId = "ApiKey"
+};
+
+var response = await  secretsManagerClient.GetSecretValueAsync(request);
+
+Console.WriteLine(response.SecretString);
+
+
+var describeRequest = new DescribeSecretRequest
+{
+    SecretId = "ApiKey"
+};
+
+var describeResponse = await secretsManagerClient.DescribeSecretAsync(describeRequest);
