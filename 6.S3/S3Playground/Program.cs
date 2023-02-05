74519cad1eb530ba75d30676a35ae64c87520166
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-02-05 16:28:34 +0300

    feat: add upload to s3 logic

--- 6.S3/S3Playground/Program.cs
+++ 6.S3/S3Playground/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine();
+﻿
+using Amazon.S3;
+using Amazon.S3.Model;
+
+var s3Client = new AmazonS3Client();
+
+// await using var inputStream = new FileStream("./face.jpg", FileMode.Open, FileAccess.Read);
+await using var inputStream = new FileStream("./movies.csv", FileMode.Open, FileAccess.Read);
+    
+/*var putObjectRequest = new PutObjectRequest
+{
+    BucketName    = "ske-aws-try",
+    Key = "images/face.jpg",
+    ContentType = "image/jpeg",
+    InputStream = inputStream
+};*/
+
+var putObjectRequest = new PutObjectRequest
+{
+    BucketName    = "ske-aws-try",
+    Key = "files/movies.csv",
+    ContentType = "text/csv",
+    InputStream = inputStream
+};
+
+await s3Client.PutObjectAsync(putObjectRequest);
